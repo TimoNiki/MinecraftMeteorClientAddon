c845f7c695808b4ce75b6278f0e46eb2ec600024
TimoNiki <295574811+TimoNiki@users.noreply.github.com>
2026-07-17 17:47:07 +0300

    Update and rename compile.cmd to fast-compile.cmd

---
+++ fast-compile.cmd
@@ -0,0 +1,20 @@
+# этот файл правильно скомпилирует проект так как
+# в нем учитываются настройки проекта и gradle
+# чтобы скомпилировать код вам потребуется Java 21
+# и тут если у вас есть эта джава она
+# будет учитываться.
+@echo off
+Echo ###########################
+echo Fast-Compile by TimoNiki
+echo Copyright (c) 2026 TimoNiki
+echo ###########################
+echo setting Java To 21.
+set "JAVA_HOME=C:\Program Files\Java\jdk-21"
+
+if not exist "%JAVA_HOME%\bin\java.exe" (
+    echo Error: code 1. See error-codes.txt for help.
+    exit /b 1
+)
+
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+./gradlew build
